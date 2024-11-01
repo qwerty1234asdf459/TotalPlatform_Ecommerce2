b4dccdadd53373002af65337f24493c926a16c71
qwerty1234asdf459 <vvv5599@gmail.com>
2024-11-01 11:45:39 +0900

    10월 1일 머지 에러 수정

--- pinguMarket/src/main/java/com/example/demo/ecommerce/MyPage/MyPageController.java
+++ pinguMarket/src/main/java/com/example/demo/ecommerce/MyPage/MyPageController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.example.demo.ecommerce.Coupon.CouponService;
 import com.example.demo.ecommerce.Entity.Coupon;
